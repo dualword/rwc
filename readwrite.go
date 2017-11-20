322fd073031d869072207436b409ba93b3462d8a
opennota <opennota@gmail.com>
2017-11-20 12:24:40 +0700

    Use little endian; implement loading from .RWC

--- readwrite.go
+++ readwrite.go
@@ -14,28 +14,32 @@
 package rwc
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"io"
+	"os"
 )
 
 // LoadFrom loads binary representation of Constructor from r.
 func (c *Constructor) LoadFrom(r io.Reader) error {
-	if err := binary.Read(r, binary.BigEndian, c.ng4[:]); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, c.ng4[:]); err != nil {
 		return err
 	}
-	if err := binary.Read(r, binary.BigEndian, c.ng3[:]); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, c.ng3[:]); err != nil {
 		return err
 	}
-	if err := binary.Read(r, binary.BigEndian, c.ng3beg[:]); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, c.ng3beg[:]); err != nil {
 		return err
 	}
-	if err := binary.Read(r, binary.BigEndian, c.ng3end[:]); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, c.ng3end[:]); err != nil {
 		return err
 	}
-	if err := binary.Read(r, binary.BigEndian, c.ng2[:]); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, c.ng2[:]); err != nil {
 		return err
 	}
-	if err := binary.Read(r, binary.BigEndian, &c.ng1); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, &c.ng1); err != nil {
 		return err
 	}
 	return nil
@@ -43,8 +47,58 @@ func (c *Constructor) LoadFrom(r io.Reader) error {
 
 // WriteTo writes binary representation of Constructor to w.
 func (c *Constructor) WriteTo(w io.Writer) (int64, error) {
-	if err := binary.Write(w, binary.BigEndian, c); err != nil {
+	if err := binary.Write(w, binary.LittleEndian, c); err != nil {
 		return 0, err
 	}
 	return int64(len(c.ng4) + len(c.ng3) + len(c.ng3beg) + len(c.ng3end) + len(c.ng2) + 4), nil
+}
+
+// LoadFromRWC loads binary representation of Constructor from an .RWC file.
+func (c *Constructor) LoadFromRWC(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+
+	const signature = "RTC 3.1 DataFile (C) 1992 OMS"
+	buf := make([]byte, 143492)
+	if _, err := io.ReadFull(r, buf[:len(signature)]); err != nil {
+		return err
+	}
+	if string(buf[:len(signature)]) != signature {
+		return errors.New("invalid file signature")
+	}
+	buf = buf[:0]
+
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if b != 0xff {
+			buf = append(buf, b)
+		} else {
+			b, err := r.ReadByte()
+			if err != nil {
+				return err
+			}
+			if b == 0xff {
+				buf = append(buf, 0xff)
+			} else {
+				buf = append(buf, 0)
+				b++
+				for b > 0 {
+					buf = append(buf, 0)
+					b--
+				}
+			}
+		}
+	}
+
+	return c.LoadFrom(bytes.NewReader(buf))
 }
